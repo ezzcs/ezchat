228c7c913584ed4fb0708dd1e375c3ff2371113d
ezzcs <ezzcs@hotmail.com>
2021-11-22 17:32:06 +0800

    update

---
+++ src/spark/gutenberg/samples/BibleWordsListReduce/src/main/scala/SimpleApp.scala
@@ -0,0 +1,30 @@
+import org.apache.spark.mllib.feature.Word2Vec
+import org.apache.spark.mllib.feature.Word2VecModel
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer}
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.sql.SQLContext
+object SimpleApp {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+    val sc = new SparkContext(conf)
+
+    val data = sc.textFile("/ezroot/download/BibleWordsListClean.txt")
+    val c1 = data.collect
+    val r1 = sc.parallelize(c1) 
+    val r2 = r1.collect 
+    val r3 = r2.map(x=>(x.substring(1,x.length-1).split(","))) 
+    //val r4= r5.reduce((x,y)=>(x))
+    val r20=r3.map(x=>(x(0),x(1)))
+    val r21 = sc.parallelize(r20)
+    //val r22=r21.reduceByKey((_ + _))
+    val r22=r21.reduceByKey((_ + ':'+ _))
+    val r23  = r22.collect
+    r23.foreach(println)
+ 
+
+}
+}
